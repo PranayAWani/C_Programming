8fa0bb4d43973e5cf531f83275ef073c1f05e360
PranayAWani <152209796+PranayAWani@users.noreply.github.com>
2023-12-30 11:24:30 +0530

    Create salary.c

---
+++ salary.c
@@ -0,0 +1,13 @@
+/*
+Author:-Pranay Ajitkumar Wani
+this code is structured in c programming to find the gross salary 
+*/
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main(){
+long int sal,hra=1000,ta=1200,da=1000;
+printf("\n Enter the Employee salary");
+scanf("%ld",&sal);
+printf("")
+}
