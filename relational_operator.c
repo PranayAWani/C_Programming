f73bcdae8c6094a27bfba3a821df1125cfb99ea9
PranayAWani <152209796+PranayAWani@users.noreply.github.com>
2023-12-30 11:20:00 +0530

    Create relational_operator.c

---
+++ relational_operator.c
@@ -0,0 +1,27 @@
+/*
+Author:- Pranay Ajitkumar Wani
+The given code is designed in c programming to check and practice the relational operators.
+*/
+#include<stdio.h>
+#include<conio.h>
+void main(){
+    int a,b,c;
+printf("\n Enter the value of A\t");
+scanf("%d",&a);
+printf("\n Enter the value of B\t");
+scanf("%d",&b);
+printf("\n Value of A=%d \t B=%d",a,b);
+c=a<b;
+printf("\n A<B = %d",c);
+c=a<=b;
+printf("\n A<=B = %d",c);
+c=a>b;
+printf("\n A>B = %d",c);
+c=a>=b;
+printf("\n A>=B = %d",c);
+c=a==b;
+printf("\n A==B = %d",c);
+c=a!=b;
+printf("\n A!=B = %d",c);
+
+}
