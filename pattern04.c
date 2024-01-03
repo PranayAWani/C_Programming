3798ae0ab58612d3fea49178a015c498e2c976a6
PranayAWani <152209796+PranayAWani@users.noreply.github.com>
2024-01-03 21:42:56 +0530

    Create pattern04.c

---
+++ pattern04.c
@@ -0,0 +1,16 @@
+// 1
+// 12
+// 123
+// 1234
+// 12345
+#include<stdio.h>
+int main(){
+    int i,j;
+    for(i=0;i<5;i++){
+        printf("\n");
+        for(j=1;j<=1;j++){
+            printf("%d",j);
+        }
+    }
+    return 0;
+}
