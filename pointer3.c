7bd3e3c70686ae001390e99b8e9619af96abcf8e
PranayAWani <152209796+PranayAWani@users.noreply.github.com>
2024-01-06 13:41:22 +0530

    Create pointer3.c

---
+++ pointer3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int swap(int *a,int *b);
+int worng_swap(int *a,int *b);
+int main(){
+int a=3,b=4;
+printf("\n the value of a  and b is %d and %d ",x,y);
+swap( &a, &b);
+printf("\n the value of a  and b is %d and %d ",x,y);
+return 0;
+}
+int swap(int *a, int *b){
+int temp;
+
+}
