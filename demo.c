30e5799f78de5f3a9430b453305b66afa5a02455
PranayAWani <pranaywani04@gmail.com>
2024-07-20 13:47:59 +0530

    go

--- demo.c
+++ demo.c
@@ -1,4 +1,12 @@
 #include<studio.h>
 int main(){
-    
+    int no;
+    printf("\n Enter a number to check its divisibility :-");
+    scanf("%d",&no);
+    if(no%7==0){
+        printf("\nnumber is divisible by 7");
+    }
+    else{
+        printf("\n Nymber is not divisible by 7");
+    }
 }
